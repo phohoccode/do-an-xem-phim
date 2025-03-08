d7f37a74bffa17ebdf9781d74b8101d11dd0ba2d
phohoccode <qviet092@gmail.com>
2025-03-09 01:55:54 +0700

    cập nhật header , xây dựng trang xem phim

--- navbar.php
+++ navbar.php
@@ -1,53 +1,126 @@
+<nav>
+  <div class="nav-left">
+    <a class="logo" href="/">VLUTE-FILM</a>
+    <ul class="nav-links">
+      <li>
+        <a href="" class="nav-item">Trang chủ</a>
+      </li>
+      <li>
+        <a href="" class="nav-item">Phim bộ</a>
+      </li>
+      <li>
+        <a href="" class="nav-item">Phim lẻ</a>
+      </li>
+      <li>
+        <a href="" class="nav-item">Hoạt hình</a>
+      </li>
+      <li class="dropdown">
+        <span class="title">Thể loại</span>
+        <ul class="sub-menu">
+          <li><a href="/the-loai/hanh-dong" class="nav-item">Hành Động</a></li>
+          <li><a href="/the-loai/mien-tay" class="nav-item">Miền Tây</a></li>
+          <li><a href="/the-loai/tre-em" class="nav-item">Trẻ Em</a></li>
+          <li><a href="/the-loai/lich-su" class="nav-item">Lịch Sử</a></li>
+          <li><a href="/the-loai/co-trang" class="nav-item">Cổ Trang</a></li>
+          <li><a href="/the-loai/chien-tranh" class="nav-item">Chiến Tranh</a></li>
+          <li><a href="/the-loai/vien-tuong" class="nav-item">Viễn Tưởng</a></li>
+          <li><a href="/the-loai/kinh-di" class="nav-item">Kinh Dị</a></li>
+          <li><a href="/the-loai/tai-lieu" class="nav-item">Tài Liệu</a></li>
+          <li><a href="/the-loai/bi-an" class="nav-item">Bí Ẩn</a></li>
+          <li><a href="/the-loai/phim-18" class="nav-item">Phim 18+</a></li>
+          <li><a href="/the-loai/tinh-cam" class="nav-item">Tình Cảm</a></li>
+          <li><a href="/the-loai/tam-ly" class="nav-item">Tâm Lý</a></li>
+          <li><a href="/the-loai/the-thao" class="nav-item">Thể Thao</a></li>
+          <li><a href="/the-loai/phieu-luu" class="nav-item">Phiêu Lưu</a></li>
+          <li><a href="/the-loai/am-nhac" class="nav-item">Âm Nhạc</a></li>
+          <li><a href="/the-loai/gia-dinh" class="nav-item">Gia Đình</a></li>
+          <li><a href="/the-loai/hoc-duong" class="nav-item">Học Đường</a></li>
+          <li><a href="/the-loai/hai-huoc" class="nav-item">Hài Hước</a></li>
+          <li><a href="/the-loai/hinh-su" class="nav-item">Hình Sự</a></li>
+          <li><a href="/the-loai/vo-thuat" class="nav-item">Võ Thuật</a></li>
+          <li><a href="/the-loai/khoa-hoc" class="nav-item">Khoa Học</a></li>
+          <li><a href="/the-loai/than-thoai" class="nav-item">Thần Thoại</a></li>
+          <li><a href="/the-loai/chinh-kich" class="nav-item">Chính Kịch</a></li>
+          <li><a href="/the-loai/kinh-dien" class="nav-item">Kinh Điển</a></li>
+        </ul>
 
+      </li>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">MOVIES-VLUTE</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" href="index.php">Trang chủ</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Danh mục</a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Phim lẻ</a></li>
-            <li><a class="dropdown-item" href="#">Phim bộ</a></li>
-            <li><a class="dropdown-item" href="#">Tv Shows</a></li>
-            <li><a class="dropdown-item" href="#">Phim Vietsub</a></li>
-            <li><a class="dropdown-item" href="#">Phim thuyết minh</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Xem thêm</a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Phim đã lưu</a></li>
-            <li><a class="dropdown-item" href="#">Lịch sử đã xem</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Tìm kiếm phim..." aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-      <div class="ms-4">
-        <?php if (isset($_SESSION['user'])): ?>
-          <a href="logout.php" class="btn btn-danger"> Đăng xuất </a>
-        <?php else: ?>
-          <a href="login.php" class="btn btn-primary"> Đăng nhập </a>
-          <a href="register.php" class="btn btn-light"> Đăng ký </a>
-        <?php endif; ?>
-      </div>
+      <li class="dropdown">
+        <span class="title">Quốc gia</span>
+        <ul class="sub-menu">
+          <li><a class="nav-item" href="/viet-nam">Việt Nam</a></li>
+          <li><a class="nav-item" href="/trung-quoc">Trung Quốc</a></li>
+          <li><a class="nav-item" href="/thai-lan">Thái Lan</a></li>
+          <li><a class="nav-item" href="/hong-kong">Hồng Kông</a></li>
+          <li><a class="nav-item" href="/phap">Pháp</a></li>
+          <li><a class="nav-item" href="/duc">Đức</a></li>
+          <li><a class="nav-item" href="/ha-lan">Hà Lan</a></li>
+          <li><a class="nav-item" href="/mexico">Mexico</a></li>
+          <li><a class="nav-item" href="/thuy-dien">Thụy Điển</a></li>
+          <li><a class="nav-item" href="/philippines">Philippines</a></li>
+          <li><a class="nav-item" href="/dan-mach">Đan Mạch</a></li>
+          <li><a class="nav-item" href="/thuy-si">Thụy Sĩ</a></li>
+          <li><a class="nav-item" href="/ukraina">Ukraina</a></li>
+          <li><a class="nav-item" href="/han-quoc">Hàn Quốc</a></li>
+          <li><a class="nav-item" href="/au-my">Âu Mỹ</a></li>
+          <li><a class="nav-item" href="/an-do">Ấn Độ</a></li>
+          <li><a class="nav-item" href="/canada">Canada</a></li>
+          <li><a class="nav-item" href="/tay-ban-nha">Tây Ban Nha</a></li>
+          <li><a class="nav-item" href="/indonesia">Indonesia</a></li>
+          <li><a class="nav-item" href="/ba-lan">Ba Lan</a></li>
+          <li><a class="nav-item" href="/malaysia">Malaysia</a></li>
+          <li><a class="nav-item" href="/bo-dao-nha">Bồ Đào Nha</a></li>
+          <li><a class="nav-item" href="/uae">UAE</a></li>
+          <li><a class="nav-item" href="/chau-phi">Châu Phi</a></li>
+          <li><a class="nav-item" href="/a-rap-xe-ut">Ả Rập Xê Út</a></li>
+          <li><a class="nav-item" href="/nhat-ban">Nhật Bản</a></li>
+          <li><a class="nav-item" href="/dai-loan">Đài Loan</a></li>
+          <li><a class="nav-item" href="/anh">Anh</a></li>
+          <li><a class="nav-item" href="/quoc-gia-khac">Quốc Gia Khác</a></li>
+          <li><a class="nav-item" href="/tho-nhi-ky">Thổ Nhĩ Kỳ</a></li>
+          <li><a class="nav-item" href="/nga">Nga</a></li>
+          <li><a class="nav-item" href="/uc">Úc</a></li>
+          <li><a class="nav-item" href="/brazil">Brazil</a></li>
+          <li><a class="nav-item" href="/y">Ý</a></li>
+          <li><a class="nav-item" href="/na-uy">Na Uy</a></li>
+          <li><a class="nav-item" href="/namh">Namh</a></li>
+          <li><a class="nav-item" href="/kinh-dien">Kinh Điển</a></li>
+        </ul>
+      </li>
+
+    </ul>
+  </div>
+  <div class="nav-center">
+    <div class="group">
+      <svg viewBox="0 0 24 24" aria-hidden="true" class="search-icon">
+        <g>
+          <path
+            d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+        </g>
+      </svg>
+
+      <input
+        id="query"
+        class="input"
+        type="search"
+        placeholder="Tìm kiếm phim..."
+        name="searchbar" />
     </div>
   </div>
+  <div class="nav-right">
+    <a href="#" style="text-decoration: none;">
+      <button class="btn-login">
+        Đăng nhập
+        <svg class="icon" viewBox="0 0 24 24" fill="currentColor">
+          <path
+            fill-rule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+            clip-rule="evenodd"></path>
+        </svg>
+      </button>
+
+    </a>
+  </div>
+
 </nav>
